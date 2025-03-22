1c02195ba81361be77412dccc3f913142a42c3d4
Aaron Longwell <a@adl.io>
2025-03-22 10:47:23 -0700

    Create eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,47 @@
+import eslint from '@eslint/js';
+import prettierConfig from 'eslint-config-prettier';
+import importPlugin from 'eslint-plugin-import';
+import prettierPlugin from 'eslint-plugin-prettier';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettierConfig,
+  {
+    plugins: {
+      prettier: prettierPlugin,
+      import: importPlugin,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      'import/order': [
+        'error',
+        {
+          groups: ['builtin', 'external', 'internal', ['parent', 'sibling'], 'index'],
+          'newlines-between': 'always',
+          alphabetize: { order: 'asc', caseInsensitive: true },
+        },
+      ],
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      'no-undef': 'off', // Disable no-undef as TypeScript handles this
+    },
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+      globals: {
+        browser: 'readonly',
+        chrome: 'readonly',
+        process: 'readonly',
+        console: 'readonly',
+      },
+    },
+    ignores: ['dist/**/*', 'node_modules/**/*', 'coverage/**/*'],
+  },
+);
