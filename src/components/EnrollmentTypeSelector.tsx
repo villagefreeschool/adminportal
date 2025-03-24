34f0570ee0c82594593c2570eda86673634dd58d
Aaron Longwell <a@adl.io>
2025-03-24 10:40:10 -0700

    style: Format EnrollmentTypeSelector.tsx with prettier

---
+++ src/components/EnrollmentTypeSelector.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { FormControl, InputLabel, MenuItem, Select, FormHelperText } from '@mui/material';
+
+/**
+ * Enrollment type selector for student attendance options
+ */
+interface EnrollmentTypeSelectorProps {
+  value: string;
+  onChange: (value: string) => void;
+  label: string;
+  required?: boolean;
+  error?: boolean;
+  helperText?: string;
+}
+
+const EnrollmentTypeSelector: React.FC<EnrollmentTypeSelectorProps> = ({
+  value,
+  onChange,
+  label,
+  required = false,
+  error = false,
+  helperText,
+}) => {
+  const choices = ['Not Attending', 'Part Time', 'Full Time'];
+
+  return (
+    <FormControl fullWidth required={required} error={error}>
+      <InputLabel>{label}</InputLabel>
+      <Select
+        value={value || ''}
+        onChange={(e) => onChange(e.target.value as string)}
+        label={label}
+      >
+        {choices.map((choice) => (
+          <MenuItem key={choice} value={choice}>
+            {choice}
+          </MenuItem>
+        ))}
+      </Select>
+      {helperText && <FormHelperText>{helperText}</FormHelperText>}
+    </FormControl>
+  );
+};
+
+export default EnrollmentTypeSelector;
