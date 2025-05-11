8a68c7128eaa00a1ad6ad3aa8d7d92ff8fb0609b
Aaron Longwell <a@adl.io>
2025-05-11 09:16:27 -0700

    chore(build): Don't open size visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,14 @@ import react from "@vitejs/plugin-react";
 import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     visualizer({
-      open: true,
+      open: false,
       gzipSize: true,
       brotliSize: true,
-      filename: "dist/stats.html",
+      filename: "dist/bundle-size-stats.html",
     }),
   ],
   server: {
