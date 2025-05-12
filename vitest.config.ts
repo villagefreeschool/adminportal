9285c6211df96b814632428cf31f67b8a0994413
Aaron Longwell <a@adl.io>
2025-05-11 22:46:09 -0700

    chore(test): Improve vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,16 +1,49 @@
 import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tsconfigPaths(), react()],
+
+  resolve: {
+    // We leave alias blank and get the values
+    // from tsconfig.compilerOptions.paths via
+    // the tsconfigPaths plugin.
+    alias: {},
+  },
+
   test: {
     globals: true,
     environment: "jsdom",
+
+    server: {
+      deps: {},
+    },
+
     setupFiles: ["./src/setupTests.ts"],
+
     coverage: {
       provider: "v8",
-      reporter: ["text", "json", "html"],
-      exclude: ["node_modules/", "src/setupTests.ts"],
+
+      exclude: [
+        "build/",
+        "coverage/",
+        "dist/",
+        "estlint.config.js",
+        "scripts/",
+        "src/__generated__",
+        "src/_tests",
+        "src/types.ts",
+        "vite.config.ts",
+        "vitest.config.ts",
+        "src/setupTests.ts",
+        ".prettierrc.ts",
+        "**/types.ts",
+      ],
+
+      reporter: ["text", "cobertura", "json", "html"],
+
+      reportsDirectory: "coverage/",
     },
   },
 });
